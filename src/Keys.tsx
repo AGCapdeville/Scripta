2b0259b1cef0cb473b6a17d50862e680f9509e9b
Adam <AGCapdeville@gmail.com>
2025-06-08 07:01:09 -0700

    Keyboard CSS fun
    Keyboard CSS fun

--- src/Keys.tsx
+++ src/Keys.tsx
@@ -2,12 +2,37 @@ import styled from 'styled-components';
 
 
 const LetterButton = styled.button`
+  width: 10vw;
+  height: 50px;
 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 14px;
+  margin: 0;
+  background-color: #a2a2a2;
+
+  border-radius: 11%;
+  border-color: #a2a2a2;
+
+  text-transform: uppercase;
+  font-weight: bold;
+  user-select: none;
+
+  @media (min-width: 500px) {
+  /* Styles for the smallest phones */
+    width: 43px;
+  }
 `;
 
 const Row = styled.div`
   display: flex;
   justify-content: center;
+  width: 100%;
+  gap: 2px;
+  padding-top: 2px;
+  padding-bottom: 2px;
 `;
 
 const Keyboard = styled.div`
