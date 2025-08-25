82af72afe86efa49f5487ccc1e5b108768f1e9c2
agcapdeville <agcapdeville@gmail.com>
2025-08-25 07:16:11 -0700

    Game screen wip

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,23 @@
 import { Link } from 'react-router-dom';
+import { PresentLetterColor } from '../types/Colors'
+
 
 export const Home = () => {
   
   // While developing.
   // localStorage.clear(); // Clear local storage for testing purposes  
   return (
-    <div>
+    <>
       <p>Select a game:</p>
-      <div className='w:100vw h:100vh flex'>
-        <Link to="/scripta/daily">Daily Word</Link>
+      <div className="flex flex-col w-full items-center content-evenly">
+        <div className="w-fit px-4 py-2 
+            text-sm font-medium 
+            bg-[#7B5CF0] 
+          text-white rounded"
+          >
+          <Link to="/scripta/daily">Daily Word</Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
